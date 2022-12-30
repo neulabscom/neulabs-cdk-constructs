fef1ac31033b6bde32b905cde75a2b69fcc1945e
FabrizioCafolla <developer@fabriziocafolla.com>
2022-12-30 12:21:16 +0100

    feat: add base tag

--- src/stacks/newrelic/index.ts
+++ src/stacks/newrelic/index.ts
@@ -1,4 +1,6 @@
-// import * as cdk from 'aws-cdk-lib';
+import { Duration, SecretValue } from 'aws-cdk-lib';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import { BaseStack, BaseStackProps } from '../base';
 
@@ -10,8 +12,54 @@ export interface NewRelicStackProps extends BaseStackProps {
 }
 
 export class NewRelicStack extends BaseStack {
+  newRelicSecret: secretsmanager.ISecret;
+  newRelicBucket: s3.IBucket;
+
   constructor(scope: Construct, id: string, props: NewRelicStackProps) {
     super(scope, id, props);
 
+    this.newRelicSecret = this.createSecrets(props.newRelicAccountId, props.newRelicLicenseKey);
+    this.newRelicBucket = this.createFirehoseBucket();
+  }
+
+
+  createSecrets(newRelicAccountId: string, newRelicLicenseLey: string) {
+    let secret = new secretsmanager.Secret(
+      this,
+      'newrelic-secret',
+      {
+        secretName: 'NEW_RELIC_LICENSE_KEY',
+        secretObjectValue: {
+          NrAccountId: SecretValue.unsafePlainText(newRelicAccountId),
+          LicenseKey: SecretValue.unsafePlainText(newRelicLicenseLey),
+        },
+      },
+    );
+    this.addBaseTag(secret);
+    return secret;
+  }
+
+  createFirehoseBucket() {
+    return new s3.Bucket(
+      this,
+      'newrelic-bucket',
+      {
+        bucketName: `neulabs-newrelic-${this.stage}-${(Math.random() + 1).toString(36).substring(7)}`,
+        versioned: true,
+        lifecycleRules: [
+          {
+            id: 'raw-bucket-lifecycle-rule',
+            enabled: true,
+            noncurrentVersionExpiration: Duration.days(30),
+            transitions: [
+              {
+                storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+                transitionAfter: Duration.days(30),
+              },
+            ],
+          },
+        ],
+      },
+    );
   }
 }
