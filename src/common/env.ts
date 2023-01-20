6e869118e2df934ea453bd8838036db5ded18b7a
FabrizioCafolla <developer@fabriziocafolla.com>
2023-01-20 13:01:49 +0100

    fix: base tag function

--- src/common/env.ts
+++ src/common/env.ts
@@ -1,8 +1,17 @@
 export const ENVIRONMENT = process.env.ENVIRONMENT ?? '';
-export const NEULABS_BUSINESS_UNIT = process.env.NEULABS_BUSINESS_UNIT ?? '';
-export const NEULABS_DOMAIN = process.env.NEULABS_DOMAIN ?? '';
-export const NEULABS_REPOSITORY = process.env.NEULABS_REPOSITORY ?? '';
-export const NEULABS_VERSION = process.env.NEULABS_VERSION ?? '';
+export const BUSINESS_UNIT = process.env.BUSINESS_UNIT ?? '';
+export const DOMAIN = process.env.DOMAIN ?? '';
+export const REPOSITORY_NAME = process.env.REPOSITORY_NAME ?? '';
+export const REPOSITORY_VERSION = process.env.REPOSITORY_VERSION ?? '';
 
 let timestamp = new Date();
-export const NEULABS_TIMESTAMP_CDK = `${timestamp.getFullYear().toString()}/${timestamp.getMonth().toString()}/${timestamp.getDay().toString()} H:${timestamp.getHours().toString()}`;
+export const TIMESTAMP_DEPLOY_CDK = `${timestamp.getFullYear().toString()}/${timestamp.getMonth().toString()}/${timestamp.getDay().toString()} H:${timestamp.getHours().toString()}`;
+
+export enum TagsKey {
+  Environment='Environment',
+  TimestampDeployCDK='TimestampDeployCDK',
+  BusinessUnit='BusinessUnit', // Indicates which business unit competes for the service released on aws
+  Domain='Domain', // Indicates the domain in which the service operates
+  RepositoryName='RepositoryName', // Indicates in which repository the released code is contained
+  RepositoryVersion='RepositoryVersion', // Indicates which version of the code was released (can be a tag or a commit hash)
+}
--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,11 +1,34 @@
 import { Tags } from 'aws-cdk-lib';
 import * as env from './env';
 
-export function addBaseTags(module: any) {
-  Tags.of(module).add('Environment', env.ENVIRONMENT);
-  Tags.of(module).add('Date', env.NEULABS_TIMESTAMP_CDK);
-  Tags.of(module).add('BusinessUnit', env.NEULABS_BUSINESS_UNIT);
-  Tags.of(module).add('Domain', env.NEULABS_DOMAIN);
-  Tags.of(module).add('Repository', env.NEULABS_REPOSITORY);
-  Tags.of(module).add('Version', env.NEULABS_VERSION);
+interface BaseTagPorps {
+  BusinessUnit?: string;
+  Domain?: string;
+  RepositoryName?: string;
+  RepositoryVersion?: string;
+}
+
+export function addBaseTags(module: any, props?: BaseTagPorps) {
+  Tags.of(module).add(env.TagsKey.Environment, env.ENVIRONMENT);
+  Tags.of(module).add(env.TagsKey.TimestampDeployCDK, env.TIMESTAMP_DEPLOY_CDK);
+
+  let businessUnit = props?.BusinessUnit ?? env.BUSINESS_UNIT;
+  if (businessUnit) {
+    Tags.of(module).add(env.TagsKey.BusinessUnit, businessUnit);
+  }
+
+  let domain = props?.Domain ?? env.DOMAIN;
+  if (domain) {
+    Tags.of(module).add(env.TagsKey.Domain, domain);
+  }
+
+  let repositoryName = props?.RepositoryName ?? env.REPOSITORY_NAME;
+  if (repositoryName) {
+    Tags.of(module).add(env.TagsKey.RepositoryName, repositoryName);
+  }
+
+  let repositoryVersion = props?.RepositoryVersion ?? env.REPOSITORY_VERSION;
+  if (repositoryVersion) {
+    Tags.of(module).add(env.TagsKey.RepositoryVersion, repositoryVersion);
+  }
 }
