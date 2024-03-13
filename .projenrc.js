79168a76d8cc6c8a8053d467cdd376094beb03a2
Andrea Pregnolato <andrea.pregnolato@neulabs.com>
2024-03-13 12:12:28 +0100

    fix: projen

--- .projenrc.js
+++ .projenrc.js
@@ -11,16 +11,21 @@ const project = new cdk.JsiiProject({
   authorOrganization: true,
   packageManager: javascript.NodePackageManager.YARN_CLASSIC,
   minNodeVersion: '18.16.0',
-  peerDeps: [
-    `aws-cdk-lib@${cdkVersion}`,
-  ],
+  peerDeps: [`aws-cdk-lib@${cdkVersion}`],
   deps: [
     `@aws-cdk/aws-apigatewayv2-alpha@${cdkVersion}-alpha.0`,
     `@aws-cdk/aws-apigatewayv2-integrations-alpha@${cdkVersion}-alpha.0`,
     `aws-cdk-lib@${cdkVersion}`,
     'constructs@^10.0.0',
   ],
-  devDeps: ['ts-node', 'prettier', 'eslint-config-prettier', 'eslint-plugin-prettier', 'standard-version', 'husky'],
+  devDeps: [
+    'ts-node',
+    'prettier',
+    'eslint-config-prettier',
+    'eslint-plugin-prettier',
+    'standard-version',
+    'husky',
+  ],
   publishToPypi: {
     distName: 'neulabs-cdk-constructs',
     module: 'neulabs_cdk_constructs',
@@ -31,5 +36,16 @@ const project = new cdk.JsiiProject({
   jsiiVersion: '5.3.27',
   pullRequestTemplate: false,
 });
+
+project.github.actions.set('actions/checkout', 'actions/checkout@v4');
+project.github.actions.set('actions/setup-node', 'actions/setup-node@v4');
+project.github.actions.set(
+  'actions/download-artifact',
+  'actions/download-artifact@v4',
+);
+project.github.actions.set(
+  'actions/upload-artifact',
+  'actions/upload-artifact@v4',
+);
 
 project.synth();
